73cdb53b9c3fe75656ea88b12d066a6d5fdf974a
hughec1329 <hughcrockford@gmail.com>
2013-09-02 16:57:44 +1000

    working on checks and balances

--- mapper.R
+++ mapper.R
@@ -23,30 +23,56 @@ res = sapply(1:length(js$records),function(i) {
 	c(	dat = datt,
 	 	lat = js$records[[i]]$latitude,
 		lon = js$records[[i]]$longitude,
-		cover = js$records[[i]]$form_values[[2]],
-		notes = js$records[[i]]$form_values[[1]])
+		cover = js$records[[i]]$form_values[[1]],
+		notes = js$records[[i]]$form_values[[2]])
 })
 rest = data.frame(t(res),stringsAsFactors = F)
 rest$dat = as.POSIXct(as.numeric(rest$dat),origin = "1970-01-01")
 rest$cover = as.numeric(rest$cover)
+
 # connect to postgis
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname="farms")
 # write table to postgis
-dbWriteTable(con,'obs',rest,row.names = F)
+dbWriteTable(con,'obs',rest)
 # add spatial points
 dbGetQuery(con,'ALTER TABLE obs ADD COLUMN geom GEOMETRY;')
 dbGetQuery(con,"UPDATE obs SET geom = ST_GeometryFromText('POINT(' || lon || ' ' || lat || ')',4326)") # have to import as long lat
 dbGetQuery(con,"UPDATE obs SET geom = ST_Transform(geom, 3111)") # then transform to projected
 dbGetQuery(con,'ALTER TABLE obs DROP COLUMN lat;')
 dbGetQuery(con,'ALTER TABLE obs DROP COLUMN lon;')
+dbGetQuery(con,'ALTER TABLE obs RENAME "row.names" to obsno;')
 # do spatial join.
 out = dbGetQuery(con, 'SELECT s.dat as date, p.fid, p.pid, s.cover, s.notes FROM juc_farms p, obs s WHERE ST_INTERSECTS(p.geom,s.geom) ORDER BY pid;')
+
 write.csv(out,file = paste(Sys.Date(),unique(out$fid), 'obs', sep = '_'),row.names = F)
 dbGetQuery(con, 'INSERT INTO observations SELECT s.dat as date, p.fid, p.pid, s.cover, s.notes, s.geom FROM juc_farms p, obs s WHERE ST_CONTAINS(p.geom,s.geom) ORDER BY pid;')
+
 dbGetQuery(con, 'DROP TABLE obs')
+
+#####
+### SHECKING
+#####
 #check results - need to do this before write DB?
 print(ifelse(nrow(out) == nrow(rest),paste('succesfully imported',nrow(rest),'paddock observations'), 'WARNING: some paddocks not matched'))
+
+int = dbGetQuery(con, 'select obsno, pid ,ST_WITHIN(o.geom,p.geom) as inside from obs o,juc_farms p;')
+success = dcast(int,obsno ~ .,sum)
+
+print(paste('succes observations: ' ,paste(success$obsno[success[,2] == 1],sep = ',', collapse =  ' ')))
+print( paste('missed observations: ' ,paste(success$obsno[success[,2] == 0],sep = ',', collapse =  ' ')))
+print( paste('double observations: ' ,paste(success$obsno[success[,2] > 1],sep = ',', collapse =  ' ')))
+
+
+dbGetQuery(con, 'SELECT select obsno, pid ,ST <- DISTANCE(o.geom,p.geom) as dist from obs o,juc_farms p ORDER BY dist;')
+
+# check for double observations
+if(sum(duplicated(out$pid)) != 0){
+	print('one or more paddocks duplicated')
+	print(out[duplicated(out$pid),])
+} else {
+	print('no duplicates detected')
+}
 
 
 # drops rows that dont have geolocation - try nearest neighbour.
